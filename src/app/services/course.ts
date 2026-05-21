dfeb27df0e56d46eb8ef6e2c463e9a1b728d5e41
Frida <fridapihlstrom@gmail.com>
2026-05-21 12:54:13 +0200

    Implementera CourseService med HttpClient och signals.

--- src/app/services/course.ts
+++ src/app/services/course.ts
@@ -1,6 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Course } from '../models/course.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Course {}
+export class CourseService {
+  // injicera HttpClient
+  private http = inject(HttpClient);
+  // privat signal
+  private coursesSignal = signal<Course[]>([]);
+  // publik signal
+  public courses = this.coursesSignal.asReadonly();
+
+  constructor() {
+    this.loadCourses();
+  }
+
+  // hämta data från public-mappen
+  private loadCourses(): void {
+    this.http.get<Course[]>('miun_courses.json').subscribe({
+      next: (data: Course[]) => {
+        this.coursesSignal.set(data);
+      },
+      error: (err: any) => {
+        console.error('Kunde inte ladda kursdatan från JSON-filen:', err);
+      }
+    });
+  }
+}
