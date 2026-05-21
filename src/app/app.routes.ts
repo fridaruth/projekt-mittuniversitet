714fc2f61b0aec2f110ef0abe81392ab95aabe15
Frida <fridapihlstrom@gmail.com>
2026-05-21 12:59:13 +0200

    Lagt till routes för kurskatalog och schema

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CourseCatalog } from './components/course-catalog/course-catalog';
+import { MySchedule } from './components/my-schedule/my-schedule';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Om man går till startsidan visas kurskatalogen
+    { path: '', component: CourseCatalog },
+    //om man går till /schema, visas ramschemat
+    { path: 'schema', component: MySchedule },
+    // catch-all som skickar tillbaka till startsidan
+    { path: '**', redirectTo: '' }
+];
